197b4ea6c8aaf37da0935f4c614aff2b3bee2475
joaopedro <joaopedromissiaia@gmail.com>
2026-05-08 22:21:13 -0300

    Home utilizada para, por enquanto, testar componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-import Link from 'next/link';
+import Cadastro from "../modules/components/Cadastro";
 
-export default function Home() {}
+export default function Home() {
+    return (
+        <main style={{ padding: '2rem' }}>
+            <h1>Visualizando o Form:</h1>
+
+            {/* Aqui você renderiza a tag do componente para ele aparecer na tela */}
+            <Cadastro />
+            {/*<ExampleNavigationMenu />*/}
+
+        </main>
+    );
+}
