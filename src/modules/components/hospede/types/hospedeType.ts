ea73adc29334ff4a636f3cc1090c4fd22ee58291
joaopedro <joaopedromissiaia@gmail.com>
2026-05-11 19:57:59 -0300

    Mudanca na logia de hospede e locador, logica removida, deixando apenas usuario

--- src/modules/components/hospede/types/hospedeType.ts
+++
@@ -1,7 +0,0 @@
-// interface que herda de pessoa
-
-import {Pessoa} from "@/src/modules/components/pessoa/types/pessoaType";
-
-export interface HospedeType extends Pessoa{
-    cpf: string;
-}
--- src/modules/components/locador/types/locadorType.ts
+++
@@ -1,9 +0,0 @@
-// interface que herda pessoa e imoveis
-// 1:n 1 locador pode possuir n imoveis
-
-import {Pessoa} from "@/src/modules/components/pessoa/types/pessoaType";
-import {ImovelType} from "@/src/modules/components/imoveis/types/imoveisType";
-
-export interface LocadorType extends Pessoa{
-    imoveis: ImovelType[];
-}
