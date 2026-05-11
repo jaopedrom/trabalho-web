199e062b8e63243fdc3aa27adf84f97c2e332531
joaopedro <joaopedromissiaia@gmail.com>
2026-05-10 21:45:04 -0300

    CSS do componente botao alterado para tailwind

--- src/modules/components/button.tsx
+++ src/modules/components/button.tsx
@@ -1,14 +1,16 @@
 'use client';
 import * as React from 'react';
 import { Button } from '@base-ui/react/button';
-import styles from './index.module.css';
 
 export default function ExampleButton() {
     const [loading, setLoading] = React.useState(false);
 
     return (
         <Button
-            className={styles.Button}
+            className="box-border flex items-center justify-center gap-1.5 h-10 px-3.5 m-0 outline-none border-none rounded-md
+            bg-gray-50 text-base font-normal leading-6 text-gray-900 select-none no-underline max-sm:text-[0.925rem] max-sm:px-2
+            hover:bg-gray-100 data-popup-open:bg-gray-100 focus-visible:relative focus-visible:outline-2 focus-visible:outline-blue-500
+            focus-visible:-outline-offset-1 disabled:opacity-50 disabled:cursor-not-allowed"
             disabled={loading}
             focusableWhenDisabled
             onClick={() => {
