85d28cb2911d066386d3aec75616a19308a93668
joaopedro <joaopedromissiaia@gmail.com>
2026-05-10 21:46:40 -0300

    Páginas de autenticação de login e cadastro atualizadas

--- src/app/autenticacao/cadastro/page.tsx
+++ src/app/autenticacao/cadastro/page.tsx
@@ -1,9 +1,42 @@
-'use client'
+"use client"
+import { CadastroForm, CadastroFormInputs} from "@/src/modules/components/CadastroForm";
+import { hospedesMock } from "@/src/modules/components/hospede/mocks/mockHospede";
+import { HospedeType } from "@/src/modules/components/hospede/types/hospedeType";
 
-import Cadastro from "@/src/modules/components/Cadastro";
+export default function CadastroPage() {
 
-export default function AuthCadastroPage() {
+    const processarCadastro = (data: CadastroFormInputs) => {
+        //verifica se cpf existe
+        const usuarioJaExiste = hospedesMock.find(h => h.cpf === data.cpf);
+
+        if (usuarioJaExiste) {
+            alert("Este CPF já está cadastrado em nosso sistema!");
+            return;
+        }
+
+        // monta objeto novoHospede no formato HospedeType gerando um ID falso
+        const novoHospede: HospedeType = {
+            id: `hospede-${Date.now()}`, // id gerado com base na data atual
+            ...data // insere os dados do formulário (nome, email, cpf, etc)
+        };
+
+        // salva no mock da dados
+        hospedesMock.push(novoHospede);
+
+        // exibe no console e mensagem de confirmacao
+        console.log("Novo hóspede cadastrado:", novoHospede);
+        alert("Cadastro realizado com sucesso!");
+    };
+
     return (
-        <Cadastro />
-    )
+        <main className="flex justify-center items-center min-h-screen bg-gray-100 py-10">
+            <div className="bg-white p-8 rounded-lg shadow-md flex flex-col items-center">
+                <h1 className="text-2xl font-bold mb-6 text-center">Crie sua Conta</h1>
+
+                {/*chamada do componente*/}
+                <CadastroForm aoEnviar={processarCadastro} />
+
+            </div>
+        </main>
+    );
 }
--- src/app/autenticacao/login/page.tsx
+++ src/app/autenticacao/login/page.tsx
@@ -1,9 +1,36 @@
 "use client"
 
-import Login from "@/src/modules/components/Login";
+import { LoginForm, LoginFormInputs} from "@/src/modules/components/Login";
+import { hospedesMock } from "@/src/modules/components/hospede/mocks/mockHospede";
 
-export default function AuthLoginPage() {
+export default function LoginPage() {
+
+    // validar dados de login
+    const processarLogin = (data: LoginFormInputs) => {
+        console.log("Dados capturados na Página:", data);
+
+        // salva dados do usuario encontrado no localstorage
+        const usuarioEncontrado = hospedesMock.find((hospede) =>
+            hospede.cpf === data.cpf && hospede.senha === data.senha
+        );
+
+        if (usuarioEncontrado) {
+            localStorage.setItem("usuarioLogado", JSON.stringify(usuarioEncontrado));
+            alert("Login realizado com sucesso! (Aqui você redirecionaria o usuário)");
+        } else {
+            alert("Usuário ou senha não encontrados!");
+        }
+    };
+
     return (
-        <Login />
-    )
+        <main className="flex justify-center items-center h-screen bg-gray-100">
+            <div className="bg-white p-8 rounded-lg shadow-md">
+                <h1 className="text-2xl font-bold mb-6 text-center">Acesse sua conta</h1>
+
+                {/*chamada do componente*/}
+                <LoginForm aoEnviar={processarLogin} />
+
+            </div>
+        </main>
+    );
 }
