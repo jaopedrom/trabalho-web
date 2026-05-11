a14329e89f7bc620055a8e9f27e570ca7e0d7edd
joaopedro <joaopedromissiaia@gmail.com>
2026-05-10 21:47:56 -0300

    Formulários de cadastro e login atualizados

--- src/modules/components/Login.tsx
+++ src/modules/components/Login.tsx
@@ -1,43 +1,35 @@
 "use client"
+import { useForm } from "react-hook-form";
+import { Button } from "@base-ui/react/button";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { useForm, SubmitHandler } from "react-hook-form";
-
-import {Button} from "@base-ui/react/button";
-import {z} from "zod";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {hospedesMock} from "@/src/modules/components/hospede/mocks/mockHospede";
-
-// schema de verificacao de erros dos inputs usando zod
+// schema de verificacao de entradas
 const loginSchema = z.object({
     cpf: z.string().min(11, "CPF inválido (mínimo de 11 dígitos)."),
     senha: z.string().min(8, "Senha deve conter no mínimo 8 caracteres"),
-})
+});
 
-type LoginFormInputs = z.infer<typeof loginSchema>;
+// extrai as informacoes dos campos
+export type LoginFormInputs = z.infer<typeof loginSchema>;
 
-export default function Login() {
+// prop que vai receber a funcao da pagina pai
+// pagina pai: /src/app/autenticacao/login/page.tsx
+interface LoginFormProps {
+    aoEnviar: (data: LoginFormInputs) => void;
+}
+
+export function LoginForm({ aoEnviar }: LoginFormProps) {
     const { register, handleSubmit, formState: { errors } } = useForm<LoginFormInputs>({
         resolver: zodResolver(loginSchema)
-    })
+    });
 
-    const onSubmit: SubmitHandler<LoginFormInputs> = (data) => {
-        console.log("Dados capturados:", data);
-        //busca de hospede no mock de dados com base no cpf e senha
-        const usuarioEncontrado = hospedesMock.find((hospedesMock) =>
-            hospedesMock.cpf === data.cpf && hospedesMock.senha === data.senha);
-
-        if (usuarioEncontrado) {
-            localStorage.setItem("usuario encontrado", JSON.stringify(usuarioEncontrado));
-        } else {
-            alert("Usario nao encontrado!")
-        }
-    }
-
+    // chama a prop ao enviar
     return (
         <form
             className="flex flex-col gap-4 w-full max-w-64"
-            onSubmit={handleSubmit(onSubmit)}>
-
+            onSubmit={handleSubmit(aoEnviar)}
+        >
             <label className="text-sm leading-5 font-bold text-gray-900">CPF</label>
             <input className="box-border pl-3.5 m-0 border border-gray-200 w-full h-10 rounded-md text-base font-normal bg-transparent text-gray-900 focus:outline-2 focus:outline-blue-500 focus:-outline-offset-1"
                    {...register("cpf")}
@@ -52,7 +44,9 @@ export default function Login() {
             {errors.senha && <p className="text-red-500 text-xs mt-1">{errors.senha.message}</p>}
 
             <Button className="box-border flex items-center justify-center h-10 px-3.5 m-0 outline-none border border-gray-200 rounded-md bg-gray-50 text-base font-normal leading-6 text-gray-900 select-none hover:not-data-disabled:bg-gray-100 active:not-data-disabled:bg-gray-200 active:not-data-disabled:shadow-[inset_0_1px_3px_var(--color-gray-200)] active:not-data-disabled:border-t-gray-300 focus-visible:outline-2 focus-visible:outline-blue-500 focus-visible:-outline-offset-1 data-disabled:text-gray-500"
-                    type="submit">Acessar</Button>
+                    type="submit">
+                Acessar
+            </Button>
         </form>
     )
 }
