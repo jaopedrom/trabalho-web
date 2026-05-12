c42b421d1a5bb8f46638bfff28771d407460c09b
joaopedro <joaopedromissiaia@gmail.com>
2026-05-12 11:26:25 -0300

    Paginas de imoveis relacionadas a usuario criadas

---
+++ src/app/usuario/[perfil]/imoveis/[slug]/page.tsx
@@ -0,0 +1,6 @@
+export default function ImovelDetalhes({ params }: { params: Promise<{ slug: string }> }) {
+    // seu código aqui
+    return (
+        <div>...</div>
+    )
+}
---
+++ src/app/usuario/[perfil]/imoveis/page.tsx
@@ -0,0 +1,5 @@
+export default function ImovelDashboard(){
+    return (
+        <h1>Imovel dashboard</h1>
+    )
+}
